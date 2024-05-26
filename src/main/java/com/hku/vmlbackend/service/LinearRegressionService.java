a671b292a7ce50a8906e427d7e505e2e74593b43
ysxu666 <70496853+ysxu666@users.noreply.github.com>
2024-05-26 21:44:53 +0800

    Update LinearRegressionService.java

--- src/main/java/com/hku/vmlbackend/service/LinearRegressionService.java
+++ src/main/java/com/hku/vmlbackend/service/LinearRegressionService.java
@@ -2,6 +2,7 @@ package com.hku.vmlbackend.service;
 
 public interface LinearRegressionService {
     void train(String[] features, String label, int epoch, String MD5);
+    String getEpochData();
 
 
 }
